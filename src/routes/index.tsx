b2a0232115e1f0b74e49ded683598babedcbe13a
jinseon choi <sttaurt@gmail.com>
2024-02-13 21:10:08 +0900

    Feat: recipes > recipes route 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import { Layout } from '../layouts'
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from '../layouts';
+import { Suspense, lazy } from 'react';
 
+const RecipesPage = lazy(() => import('../pages/recipes'));
 
 export default function AppRoutes() {
   return (
+    <Suspense fallback={<p>Loading...</p>}>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route
+          path='/'
+          element={<Layout />}>
+          <Route
+            path='/recipes'
+            element={<RecipesPage />}></Route>
         </Route>
-        <Route path='*' element={<p>Not Found</p>} />
-      </Routes>  )
+        <Route
+          path='*'
+          element={<p>Not Found</p>}
+        />
+      </Routes>
+    </Suspense>
+  );
 }
