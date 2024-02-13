ceb7730b62d6d7698deee7dd028f9b9d3b79703c
yunjikwak <mia007478@gmail.com>
2024-02-13 22:44:58 +0900

    feat: 임시 라우터

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,15 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}></Route>
+        <Route path="/recipes"></Route>
+        <Route path="/recipes/write"></Route>
+        <Route path="/recipes/:recipeNo/detail"></Route>
+        <Route path="/community"></Route>
+        <Route path="/community/write"></Route>
+        <Route path="/community/:recipeNo/detail"></Route>
+        <Route path="/login" />
+        <Route path="/signup" />
+        <Route path="/mypage" />
       </Route>
       <Route path="*" element={<p>Not Found</p>} />
     </Routes>
