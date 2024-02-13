95cd8bf76ecf8bf90f919e37c4d1ff02b8aa79c6
jinseon choi <sttaurt@gmail.com>
2024-02-13 21:25:41 +0900

    Feat: layout > contents > flex, justify-content 설정 추가

--- src/components/layouts/Contents.tsx
+++ src/components/layouts/Contents.tsx
@@ -5,8 +5,11 @@ import { Outlet } from 'react-router-dom';
 const ContentsWrapper = styled.div`
   padding: 70px 150px 80px 150px;
   min-width: 1200px;
-  height: calc(100vh - 80px);
-  background-color: aliceblue;
+  min-height: calc(100vh - 80px);
+  background-color: #eef0ed;
+
+  display: flex;
+  justify-content: center;
 `;
 
 export const Contents = () => {
