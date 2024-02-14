509757d78634847d784b64d8347a7e4a322e1550
jinseon choi <sttaurt@gmail.com>
2024-02-14 22:26:59 +0900

    Feat: routes > recipes detail 페이지 라우트 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,54 @@
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "../layouts";
-import { Suspense, lazy } from "react";
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from '../layouts';
+import { Suspense, lazy } from 'react';
 
-const Home = lazy(() => import("../pages/Home"));
-const RecipesPage = lazy(() => import("../pages/recipes"));
-const RecipeWritePage = lazy(() => import("../pages/recipes/write"));
-const Login = lazy(() => import("../pages/Login"));
-const Mypage = lazy(() => import("../pages/Mypage"));
-const SignUp = lazy(() => import("../pages/Signup"));
+const Home = lazy(() => import('../pages/Home'));
+const RecipesPage = lazy(() => import('../pages/recipes'));
+const RecipeDetailPage = lazy(() => import('../pages/recipes/detail'));
+const RecipeWritePage = lazy(() => import('../pages/recipes/write'));
+const Login = lazy(() => import('../pages/Login'));
+const Mypage = lazy(() => import('../pages/Mypage'));
+const SignUp = lazy(() => import('../pages/Signup'));
 
 export default function AppRoutes() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}></Route>
-          <Route path="/recipes" element={<RecipesPage />}></Route>
-          <Route path="/recipes/write" element={<RecipeWritePage />}></Route>
-          <Route path="/recipes/:recipeNo/detail"></Route>
-          <Route path="/community"></Route>
-          <Route path="/community/write"></Route>
-          <Route path="/community/:recipeNo/detail"></Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/mypage" element={<Mypage />} />
+        <Route
+          path='/'
+          element={<Layout />}>
+          <Route
+            index
+            element={<Home />}></Route>
+          <Route
+            path='/recipes'
+            element={<RecipesPage />}></Route>
+          <Route
+            path='/recipes/write'
+            element={<RecipeWritePage />}></Route>
+          <Route
+            path='/recipes/:recipeNo/detail'
+            element={<RecipeDetailPage />}></Route>
+          <Route path='/community'></Route>
+          <Route path='/community/write'></Route>
+          <Route path='/community/:recipeNo/detail'></Route>
+          <Route
+            path='/login'
+            element={<Login />}
+          />
+          <Route
+            path='/signup'
+            element={<SignUp />}
+          />
+          <Route
+            path='/mypage'
+            element={<Mypage />}
+          />
         </Route>
-        <Route path="*" element={<p>Not Found</p>} />
+        <Route
+          path='*'
+          element={<p>Not Found</p>}
+        />
       </Routes>
     </Suspense>
   );
